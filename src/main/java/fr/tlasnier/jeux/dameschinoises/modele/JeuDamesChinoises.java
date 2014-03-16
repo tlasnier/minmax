b163623bf5aaeb39376a8c6365780aed5dbcd8ab
tlasnier <thibault .lasnier@gmail.com>
2014-03-16 06:20:28 +0100

    JeuDamesChinoises constructeur, joueur courant, clone.

---
+++ src/main/java/fr/tlasnier/jeux/dameschinoises/modele/JeuDamesChinoises.java
@@ -0,0 +1,61 @@
+package fr.tlasnier.jeux.dameschinoises.modele;
+
+import fr.tlasnier.minmax.Clonable;
+import fr.tlasnier.minmax.JeuIA;
+import fr.tlasnier.minmax.Joueur;
+
+import java.util.List;
+
+/**
+ * Created by Thibault on 16/03/14.
+ */
+public class JeuDamesChinoises implements JeuIA<CoupDamesChinoises, JoueurDamesChinoises> {
+    private PlateauDamesChinoises plateau = new PlateauDamesChinoises();
+    private JoueurDamesChinoises joueur1 = new JoueurDamesChinoises(DamesChinoises.ROUGE);
+    private JoueurDamesChinoises joueur2 = new JoueurDamesChinoises(DamesChinoises.BLEU);
+    private boolean joueurCourant = true;
+
+    public JeuDamesChinoises() {}
+
+    public JeuDamesChinoises(JeuDamesChinoises jeuOriginal) {
+       plateau = (PlateauDamesChinoises) jeuOriginal.plateau.getClone();
+       joueurCourant = jeuOriginal.joueurCourant;
+    }
+
+    @Override
+    public JeuIA jouerLeCoup(CoupDamesChinoises coup) {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public boolean estFini() {
+        //TODO
+        return false;
+    }
+
+    @Override
+    public List<CoupDamesChinoises> listerLesCoups() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public JoueurDamesChinoises getJoueurCourant() {
+        if (joueurCourant)
+            return joueur1;
+        else
+            return joueur2;
+    }
+
+    @Override
+    public Clonable getClone() {
+        return new JeuDamesChinoises(this);
+    }
+
+    @Override
+    public double evaluer(Joueur joueur) {
+        //TODO
+        return 0;
+    }
+}
